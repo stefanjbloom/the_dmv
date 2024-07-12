b1bceef7251f5f31d5af578531ebccae40021b25
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-11 20:23:08 -0600

    Feat: registrant has name

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,13 +2,31 @@ require 'spec_helper'
 
 RSpec.describe Registrant do
 
+    before(:each) do
+        registrant = Registrant.new(@name, @age, @permit)
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant.name).to eq(@name)
+        expect(registrant_1.name).to eq('Bruce')
+        expect(registrant_2.name).to eq('Penny')
+
+    end
+
     it 'exists' do
         registrant = Registrant.new(@name, @age, @permit)
         expect(registrant).to be_instance_of(Registrant)
     end
 
+    it 'has a name' do
+        registrant = Registrant.new(@name, @age, @permit)
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant.name).to eq(@name)
+        expect(registrant_1.name).to eq('Bruce')
+        expect(registrant_2.name).to eq('Penny')
+    end
+
 end
 
-        # @registrant_1 = Registrant.new('Bruce', 18, true )
-        # @registrant_2 = Registrant.new('Penny', 15 )
-        # @registrants = [@registrant_1, @registrant_2]
