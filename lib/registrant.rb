f4ba066751693d3f38d86474d3eed435f7b40b15
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-11 20:06:00 -0600

    Feat: confirm registrant object exists

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,5 +1,15 @@
 class Registrant
 
+    attr_reader :name,
+                :age,
+                :permit,
+                :license_data
+
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+    end
 
 # We are defining this registrant class so that visitors can use our services.  
 # The registrant should have a name, age, permit, and license_data.
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Registrant do
-    
+
+    it 'exists' do
+        registrant = Registrant.new(@name, @age, @permit)
+        expect(registrant).to be_instance_of(Registrant)
+    end
+
 end
+
+        # @registrant_1 = Registrant.new('Bruce', 18, true )
+        # @registrant_2 = Registrant.new('Penny', 15 )
+        # @registrants = [@registrant_1, @registrant_2]
